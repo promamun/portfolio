f954df566850ba13e77e772fade727c6c5506164
Farhan Ahmed <farhanahmed6997@gmail.com>
2023-07-21 10:43:20 +0530

    project is done

---
+++ src/components/projects/Data/data.js
@@ -0,0 +1,51 @@
+const ProjectList = [
+  {
+    image: "images/gallery/6.jpg",
+    title: "CMS",
+    title1: "Construction  Management Process"
+  },
+  {
+    image: "images/gallery/7.jpg",
+    title: "Digital Work",
+    title1: "Construction  Management Process"
+  },
+  {
+    image: "images/gallery/8.jpg",
+    title: "Mobile App Vister",
+    title1: "Construction  Management Process"
+  },
+  {
+    image: "images/gallery/9.jpg",
+    title: "Opertray",
+    title1: "Construction  Management Process"
+  },
+  {
+    image: "images/gallery/10.jpg",
+    title: "Innovative Interface",
+    title1: "Construction  Management Process"
+  },
+  {
+    image: "images/gallery/11.jpg",
+    title: "Interface",
+    title1: "Construction  Management Process"
+  },
+  {
+    image: "images/gallery/12.jpg",
+    title: "Mobile App",
+    title1: "Construction  Management Process"
+  },
+  {
+    image: "images/gallery/13.jpg",
+    title: "Strategy",
+    title1: "Construction  Management Process"
+  }
+ 
+ 
+ 
+ 
+ 
+ 
+  
+];
+
+export default ProjectList;
