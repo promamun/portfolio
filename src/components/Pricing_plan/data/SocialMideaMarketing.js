153197559ab6d065f8c1a41d664a17cd7e794a96
Farhan Ahmed <farhanahmed6997@gmail.com>
2023-07-24 17:19:34 +0530

    pricing  is full complete

--- src/components/Pricing_plan/data/SocialMideaMarketing.js
+++ src/components/Pricing_plan/data/SocialMideaMarketing.js
@@ -8,35 +8,29 @@ const SocialMarketing = [
     thumb2Keywords: "Two Social Account Setup & manage",
     thumb2Social: "Graphically Banner, Post Create",
     thumb2Optimize: "Per month 15 content create and post",
-    thumb2Support: "Ads Campaign Run & for 3 days",
-    pricingTitle: "Increase Likes Followers",
-    packageTitle: "Increase Engagements day by day"
+    thumb2Support: "Ads Campaign Run & for 3 days"
   },
   {
     backgroundImage: "url('images/resource/price-shape.png')",
-    thumb2Title: "Starting - CMS",
-    thumb2Price: "$300",
-    thumb2Month: "per month",
-    thumb2Analytics: "Website Starter with template-based design",
-    thumb2Keywords: "Up to 5 pages website a unique look",
-    thumb2Social: "Basic websites with simple functionality",
-    thumb2Optimize: "Fully Mobile Compatiblen",
-    thumb2Support: "24/7 Support",
-    pricingTitle: "Our Pricing Plans",
-    packageTitle: "Website Creation"
+    thumb2Title: "Better Plan",
+    thumb2Price: "$700",
+    thumb2Month: "/MONTH",
+    thumb2Analytics: "Facebook - Instagram - Twitter - Any Social Account",
+    thumb2Keywords: "Two Social Account Setup & manag",
+    thumb2Social: "Graphically Banner, Post Create",
+    thumb2Optimize: "Per month 25 content create and post",
+    thumb2Support: "Ads Campaign Run & for 7 days"
   },
   {
     backgroundImage: "url('images/resource/price-shape-2.png')",
-    thumb2Title: "Starting - CMS",
-    thumb2Price: "$300",
-    thumb2Month: "per month",
-    thumb2Analytics: "Website Starter with template-based design",
-    thumb2Keywords: "Up to 5 pages website a unique look",
-    thumb2Social: "Basic websites with simple functionality",
-    thumb2Optimize: "Fully Mobile Compatiblen",
-    thumb2Support: "24/7 Support",
-    pricingTitle: "Our Pricing Plans",
-    packageTitle: "Website Creation"
+    thumb2Title: "Best Plan",
+    thumb2Price: "$1000",
+    thumb2Month: "/MONTH",
+    thumb2Analytics: "Facebook - Instagram - Twitter - Any Social Account",
+    thumb2Keywords: "Two Social Account Setup & manage",
+    thumb2Social: "Graphically Banner, Post Create",
+    thumb2Optimize: "Per month 35 content create and post",
+    thumb2Support: "Ads Campaign Run & for 15 days"
   }
 ];
 export default SocialMarketing;
