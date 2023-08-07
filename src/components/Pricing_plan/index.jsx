9cd700a1c9c38c296cf9f06b35de3852c3d11e5b
Farhan Ahmed <farhanahmed6997@gmail.com>
2023-08-07 16:33:33 +0530

    shoify,wordpress&laravel done

--- src/components/Pricing_plan/index.jsx
+++ src/components/Pricing_plan/index.jsx
@@ -4,6 +4,10 @@ import BroadCamp from "../broadCamp/BroadCamp";
 import { Helmet } from "react-helmet";
 import PriceCategoryTwo from "./smMarketing";
 import PriceCategoryThree from "./PaidAdsMarketing";
+import WordPress from "./wordpress/WordPress";
+import Shopify from "./shopify/Shopify";
+import Laravel from "./laravel/Laravel";
+
 export default function index() {
   const BgImage = {
     backgroundImage: `url(images/background/22.png)`
@@ -18,8 +22,11 @@ export default function index() {
         pricingTitle="Our Pricing Plans"
         packageTitle="Website Creation"
       />
-      <PriceCategoryTwo packageTitle="Social Media Marketing"/>
-      <PriceCategoryThree packageTitle="Paid Ads Marketing"/>
+      <PriceCategoryTwo packageTitle="Social Media Marketing" />
+      <PriceCategoryThree packageTitle="Paid Ads Marketing" />
+      <WordPress packageTitle="Word Press" />
+      <Shopify packageTitle="Shopify" />
+      <Laravel packageTitle="Laravel" />
     </div>
   );
 }
