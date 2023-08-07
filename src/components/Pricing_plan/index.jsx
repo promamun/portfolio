f3f83b4b06d3ab4d07e267045d3007f346cdd48c
Mamun Pro <pro.mamun.616@gmail.com>
2023-08-07 23:50:35 +0600

    service done

--- src/components/Pricing_plan/index.jsx
+++ src/components/Pricing_plan/index.jsx
@@ -3,7 +3,7 @@ import PricingPlans from "./websiteCreation";
 import BroadCamp from "../broadCamp/BroadCamp";
 import { Helmet } from "react-helmet";
 import CMS from "./websiteCMSCreation";
-import PriceCategoryTwo from "./smMarketing";
+import FBMarketing from "./smMarketing";
 import GoogleAds from "./GoogleAds";
 import PriceCategoryThree from "./PaidAdsMarketing";
 import WordPress from "./wordpress/WordPress";
@@ -22,13 +22,10 @@ export default function index() {
       <BroadCamp name="Pricing Plans" bgImageStyle={BgImage} />
       <PricingPlans pricingTitle="Our Pricing Plans" packageTitle="React Web App Development (MERN)"/>
         <CMS packageTitle="WordPress Web Application Development(CMS)"/>
-      <PriceCategoryTwo packageTitle="Facebook Marketing"/>
+        <Shopify packageTitle="Shopify Store Design(CMS)"/>
+        <Laravel packageTitle="Laravel Web Application Development(PHP)" />
+      <FBMarketing packageTitle="Facebook Marketing"/>
       <GoogleAds packageTitle="Google Ads Marketing"/>
-      <PriceCategoryTwo packageTitle="Social Media Marketing" />
-      <PriceCategoryThree packageTitle="Paid Ads Marketing" />
-      <WordPress packageTitle="Word Press" />
-      <Shopify packageTitle="Shopify" />
-      <Laravel packageTitle="Laravel" />
     </div>
   );
 }
